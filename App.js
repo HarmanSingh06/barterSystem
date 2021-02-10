96fbf627c0b55e15e20274d1249e4f6fcf733051
Harman Singh <singhharman160201@gmail.com>
2021-02-10 22:38:09 +0530

    made a navigator

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import * as React from 'react';
 import {Text,View} from 'react-native';
-import WelcomeScreen from './screens/welcomeScreen.js'
+import WelcomeScreen from './screens/welcomeScreen.js';
+import {AppTabNavigator} from './components/AppTabNavigator';
+import {createAppContainer,createSwitchNavigator} from 'react-navigation';
 
 
 export default class App extends React.Component{
   render(){
     return(
-      <WelcomeScreen/>
+      <AppContainer/>
     )
   }
 }
+const SwitchNavigator = createSwitchNavigator({
+  WelcomeScreen:{screen:WelcomeScreen},
+  TabNavigator:{screen:AppTabNavigator}
+})
+const AppContainer = createAppContainer(SwitchNavigator)
